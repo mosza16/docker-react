06284aed8fada8696c787412dcca5dc4d416d00a
kittipong_kam@true-e-logistics.com <kittipong_kam@true-e-logistics.com>
2021-01-27 12:02:01 +0700

    change app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> testdasdassadas
+          Edit <code>src/App.js</code> hi there!!!
         </p>
         <a
           className="App-link"
